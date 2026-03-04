efa7aeade2c2b329a9816de75cef0ab161cfa0f7
D-luan <dluan1285@gmail.com>
2026-03-03 23:00:52 -0300

    feat(etl): adiciona extração de dados a partir dos microdados do enem 2023

---
+++ etl/extract.py
@@ -0,0 +1,31 @@
+import polars as pl
+
+def extrair_dados_enem():
+    caminho_arquivo = "./data/MICRODADOS_ENEM_2023.csv"
+
+    df_extraido = pl.read_csv(
+        caminho_arquivo,
+        separator=";",
+        encoding="latin1",
+        columns=[
+            "NU_INSCRICAO",
+            "TP_PRESENCA_CN",
+            "TP_PRESENCA_CH",
+            "TP_PRESENCA_LC",
+            "TP_PRESENCA_MT",
+            "NU_NOTA_CN",
+            "NU_NOTA_CH",
+            "NU_NOTA_LC",
+            "NU_NOTA_MT",
+            "NU_NOTA_REDACAO",
+            "Q006"
+        ]
+    )
+
+    return df_extraido
+
+if __name__ == "__main__":
+    df = extrair_dados_enem()
+
+    print("Extração concluída com sucesso!")
+    print(df.head())
